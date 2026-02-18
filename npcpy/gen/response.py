bc1470088c794f8bc12e97bc4ff9452e6ef20e04
cagostino <cjp.agostino@gmail.com>
2026-02-18 13:24:11 -0500

    thinking fix

--- npcpy/gen/response.py
+++ npcpy/gen/response.py
@@ -1367,7 +1367,7 @@ def get_litellm_response(
             if key in [
                 "stop", "temperature", "top_p", "max_tokens", "max_completion_tokens",
                  "extra_headers", "parallel_tool_calls",
-                "response_format", "user", "timeout",
+                "response_format", "user", "timeout", "thinking", "reasoning_effort",
             ]:
                 api_params[key] = value
 
